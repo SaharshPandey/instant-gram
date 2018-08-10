095e38d062a047c1d00ef9e1e73fe7e61fd251cb
saharsh <saharshpandeybolt@gmail.com>
2018-08-10 17:32:26 +0530

    instantgram

--- app/src/main/java/com/example/iknownothing/instantgram/MainActivity.java
+++ app/src/main/java/com/example/iknownothing/instantgram/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.iknownothing.instantgram;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent i=new Intent();
+        startActivity(i);
     }
 }
