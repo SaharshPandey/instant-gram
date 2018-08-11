667eb129ddb13d33b0f008a874f814e07cdcca33
saharsh <saharshpandeybolt@gmail.com>
2018-08-11 17:23:00 +0530

    Created & Designed LoginActivity XML and RegisterActivity XML

--- app/src/main/java/com/example/iknownothing/instantgram/LoginActivity.java
+++ app/src/main/java/com/example/iknownothing/instantgram/LoginActivity.java
@@ -3,11 +3,11 @@ package com.example.iknownothing.instantgram;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RegisterActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_login);
     }
 }
