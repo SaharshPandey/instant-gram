c2d8fd666f915226845c4d2884199c09b4092e62
saharsh <saharshpandeybolt@gmail.com>
2018-08-12 02:16:03 +0530

    Added few Comments in MainActivity

--- app/src/main/java/com/example/iknownothing/instantgram/MainActivity.java
+++ app/src/main/java/com/example/iknownothing/instantgram/MainActivity.java
@@ -2,15 +2,14 @@ package com.example.iknownothing.instantgram;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
