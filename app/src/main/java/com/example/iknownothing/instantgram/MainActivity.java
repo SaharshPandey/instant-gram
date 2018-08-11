d9193744d0e0568d07875bf43825777a388f159f
saharsh <saharshpandeybolt@gmail.com>
2018-08-11 16:00:29 +0530

    Added DrawableLayout,NavigationBar & Toolbar

--- app/src/main/java/com/example/iknownothing/instantgram/MainActivity.java
+++ app/src/main/java/com/example/iknownothing/instantgram/MainActivity.java
@@ -1,15 +1,86 @@
 package com.example.iknownothing.instantgram;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private RecyclerView postList;
+    private Toolbar mToolbar;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Home");
+
+
+        drawerLayout = findViewById(R.id.drawable_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this,drawerLayout,R.string.drawer_open,R.string.drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        navigationView = findViewById(R.id.navigation_view);
+        View view =navigationView.inflateHeaderView(R.layout.navigation_header);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                UserItemSelector(item);
+                return false;
+            }
+
+            private void UserItemSelector(MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.nav_profile:
+                        Toast.makeText(MainActivity.this,"Profile",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_home:
+                        Toast.makeText(MainActivity.this,"Home",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_friends:
+                        Toast.makeText(MainActivity.this,"Friend List",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_find_friends:
+                        Toast.makeText(MainActivity.this,"Find Firends",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_messages:
+                        Toast.makeText(MainActivity.this,"Messages",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_settings:
+                        Toast.makeText(MainActivity.this,"Settings",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_Logout:
+                        Toast.makeText(MainActivity.this,"Logout",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+
     }
+    @Override
+public boolean onOptionsItemSelected(MenuItem item)
+{
+    if(actionBarDrawerToggle.onOptionsItemSelected(item))
+    {return true;}
+return super.onOptionsItemSelected(item);
+}
 }
