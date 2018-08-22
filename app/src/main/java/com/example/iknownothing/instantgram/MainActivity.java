6f255750be0fa3903c02ce2609c439d2367812b0
saharsh <saharshpandeybolt@gmail.com>
2018-08-22 23:53:44 +0530

    Changed Dialog Box Styles

--- app/src/main/java/com/example/iknownothing/instantgram/MainActivity.java
+++ app/src/main/java/com/example/iknownothing/instantgram/MainActivity.java
@@ -278,7 +278,7 @@ public class MainActivity extends AppCompatActivity {
                         final EditText inputfield = new EditText(MainActivity.this);
                         //STYLING
 
-                        inputfield.setPadding(10,10,10,10);
+                        inputfield.setPadding(10,50,10,10);
                         inputfield.setMaxLines(8);
                         inputfield.setText(description);
                         inputfield.setHint("Put your Caption here ");
