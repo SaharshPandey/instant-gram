f4615243695a3ff449a2b0755a34206db9e3bd42
saharsh <saharshpandeybolt@gmail.com>
2018-08-28 12:42:35 +0530

    caption will show in the post activity

--- app/src/main/java/com/example/iknownothing/instantgram/PostActivity.java
+++ app/src/main/java/com/example/iknownothing/instantgram/PostActivity.java
@@ -88,6 +88,7 @@ public class PostActivity extends AppCompatActivity {
         SelectCaption = findViewById(R.id.text_post);
         PostImage = findViewById(R.id.add_new_post_button);
 
+        SelectCaption.setText(getIntent().getStringExtra("caption"));
        /* SelectPostImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
