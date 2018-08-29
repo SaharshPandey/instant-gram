694afc24d9a8be0d49fbdb212c7ff21a3f98b364
saharsh <saharshpandeybolt@gmail.com>
2018-08-29 18:59:12 +0530

    Added Find Friends Activity

---
+++ app/src/main/java/com/example/iknownothing/instantgram/find_friends_activity.java
@@ -0,0 +1,13 @@
+package com.example.iknownothing.instantgram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class find_friends_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_friends_activity);
+    }
+}
