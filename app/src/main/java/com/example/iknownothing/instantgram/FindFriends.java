0855ea8f10c5523c2086de37a3aae8dd98d2485a
saharsh <saharshpandeybolt@gmail.com>
2018-08-30 17:02:10 +0530

    added getter and setters FindFriends class

--- app/src/main/java/com/example/iknownothing/instantgram/FindFriends.java
+++ app/src/main/java/com/example/iknownothing/instantgram/FindFriends.java
@@ -3,10 +3,36 @@ package com.example.iknownothing.instantgram;
 public class FindFriends {
     public String profileImage,fullname,username;
 
-    public FindFriends(String profileImage,String fullname,String username)
+    public FindFriends(String profileImage, String fullname, String username)
     {
+
         this.profileImage = profileImage;
         this.fullname = fullname;
         this.username = username;
     }
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
 }
