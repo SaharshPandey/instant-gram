b4e7bcd71d224d3b38fc60a7cec1d78ddb8ed72a
saharsh <saharshpandeybolt@gmail.com>
2018-08-30 11:06:32 +0530

    find friends layout validations

--- app/src/main/java/com/example/iknownothing/instantgram/find_friends_activity.java
+++ app/src/main/java/com/example/iknownothing/instantgram/find_friends_activity.java
@@ -2,6 +2,7 @@ package com.example.iknownothing.instantgram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -17,6 +18,9 @@ public class find_friends_activity extends AppCompatActivity {
         setContentView(R.layout.activity_find_friends_activity);
 
         SearchList = findViewById(R.id.searchlist);
+        SearchList.setHasFixedSize(true);
+        SearchList.setLayoutManager(new LinearLayoutManager(this));
+
         SearchInputText = findViewById(R.id.search_friends);
         Back = findViewById(R.id.going_back1);
     }
