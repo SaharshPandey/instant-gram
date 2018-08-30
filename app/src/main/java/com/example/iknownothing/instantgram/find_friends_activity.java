f0861c3d727e484ac47c269d2b8ad3e2df7215ef
saharsh <saharshpandeybolt@gmail.com>
2018-08-31 01:49:12 +0530

    recyclerview changes

--- app/src/main/java/com/example/iknownothing/instantgram/find_friends_activity.java
+++ app/src/main/java/com/example/iknownothing/instantgram/find_friends_activity.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
@@ -17,6 +19,9 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class find_friends_activity extends AppCompatActivity {
 
@@ -67,14 +72,22 @@ public class find_friends_activity extends AppCompatActivity {
             @Override
             protected void onBindViewHolder(@NonNull FindFriendsViewHolder holder, int position, @NonNull FindFriends model) {
 
+
             }
 
             @NonNull
             @Override
             public FindFriendsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                return null;
+
+                View view  = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.all_user_display_layout,parent,false);
+
+                return new FindFriendsViewHolder(view);
             }
         };
+
+        //Setting Adapter into RecyclerView
+        SearchList.setAdapter(firebaseRecyclerAdapter);
     }
 
     public static class FindFriendsViewHolder extends RecyclerView.ViewHolder
@@ -85,6 +98,23 @@ public class find_friends_activity extends AppCompatActivity {
         {
             super(itemView);
             mView = itemView;
+            }
+
+            public void setFullname(String fullname)
+            {
+                TextView name = mView.findViewById(R.id.searched_name);
+                name.setText(fullname);
+            }
+            public void setProfileImage(String profileImage)
+            {
+                CircleImageView imageView = mView.findViewById(R.id.searched_profileimage);
+                Picasso.get().load(profileImage).into(imageView);
+            }
+            public void setUsername(String username)
+            {
+                TextView name = mView.findViewById(R.id.searched_username);
+                name.setText(username);
+
             }
     }
 }
