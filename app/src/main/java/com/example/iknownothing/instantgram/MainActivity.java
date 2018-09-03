64402757dd0b08c9a133007423f44ed478396846
saharsh <saharshpandeybolt@gmail.com>
2018-09-03 23:53:58 +0530

    Created method setLikeButtonStatus

--- app/src/main/java/com/example/iknownothing/instantgram/MainActivity.java
+++ app/src/main/java/com/example/iknownothing/instantgram/MainActivity.java
@@ -623,7 +623,7 @@ public class MainActivity extends AppCompatActivity {
                     if(dataSnapshot.child(PostKey).hasChild(currentuserId))
                     {
                         coutnlikes = (int) dataSnapshot.child(PostKey).getChildrenCount();
-
+                        
                     }
                 }
 
