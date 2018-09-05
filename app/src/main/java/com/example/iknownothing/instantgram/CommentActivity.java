b4ff1e91d05460fa4269a550c1a14293b7a7d397
saharsh <saharshpandeybolt@gmail.com>
2018-09-05 10:03:36 +0530

    Comments Activity

--- app/src/main/java/com/example/iknownothing/instantgram/CommentActivity.java
+++ app/src/main/java/com/example/iknownothing/instantgram/CommentActivity.java
@@ -2,6 +2,7 @@ package com.example.iknownothing.instantgram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -12,7 +13,7 @@ public class CommentActivity extends AppCompatActivity {
 
     private CircleImageView CommentProfile;
     private EditText WriteComment;
-    private RecyclerView ShowComments;
+    private RecyclerView CommentList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +21,11 @@ public class CommentActivity extends AppCompatActivity {
         //Initialising objects;
         CommentProfile = findViewById(R.id.comment_photo);
         WriteComment = findViewById(R.id.write_comment);
-        ShowComments = findViewById(R.id.show_comments);
 
+        CommentList = findViewById(R.id.show_comments);
+        CommentList.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
     }
 }
