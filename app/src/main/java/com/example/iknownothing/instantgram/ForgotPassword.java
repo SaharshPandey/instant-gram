33bcf8c4f6c1bf8adaa90656d03615b7f25335eb
saharsh <saharshpandeybolt@gmail.com>
2018-09-07 22:27:31 +0530

    ForgotPassword Activity Validations

--- app/src/main/java/com/example/iknownothing/instantgram/ForgotPassword.java
+++ app/src/main/java/com/example/iknownothing/instantgram/ForgotPassword.java
@@ -2,12 +2,28 @@ package com.example.iknownothing.instantgram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ForgotPassword extends AppCompatActivity {
 
+    private EditText ForgotEmail;
+    private Button ForgotButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        //Defining Views
+        ForgotEmail = findViewById(R.id.forgot_email);
+        ForgotButton = findViewById(R.id.forgot_button);
+
+        ForgotButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
