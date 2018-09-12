9cef7fd69ca8e649e8fae1707ffaadcb4d8266aa
saharsh <saharshpandeybolt@gmail.com>
2018-09-12 23:20:31 +0530

    Added FriendRequestsActivity

---
+++ app/src/main/java/com/example/iknownothing/instantgram/FriendRequestsActivity.java
@@ -0,0 +1,13 @@
+package com.example.iknownothing.instantgram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FriendRequestsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_requests);
+    }
+}
