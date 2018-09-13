e6e56731a4a425621e6f53819ba8aa3251914ebd
saharsh <saharshpandeybolt@gmail.com>
2018-09-13 21:08:44 +0530

    Added Recycler View Object

--- app/src/main/java/com/example/iknownothing/instantgram/FriendRequestsActivity.java
+++ app/src/main/java/com/example/iknownothing/instantgram/FriendRequestsActivity.java
@@ -2,11 +2,14 @@ package com.example.iknownothing.instantgram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.w3c.dom.Text;
 
@@ -20,7 +23,7 @@ public class FriendRequestsActivity extends AppCompatActivity {
     private TextView fullname;
     private TextView username;
     private RecyclerView RequestList;
-    private DatabaseReference UserRef,PostRef;
+    private DatabaseReference UserRef,PostRef,RequestRef;
     private FirebaseAuth mAuth;
     String PostKey,CurrentUserId;
 
@@ -30,5 +33,19 @@ public class FriendRequestsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_requests);
+
+        mAuth  = FirebaseAuth.getInstance();
+        CurrentUserId = mAuth.getCurrentUser().getUid();
+
+        UserRef = FirebaseDatabase.getInstance().getReference("Users");
+        RequestRef = FirebaseDatabase.getInstance().getReference("Users").child("FriendRequests");
+
+        RequestList = findViewById(R.id.friend_requests);
+        RequestList.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        RequestList.setLayoutManager(linearLayoutManager);
+
     }
 }
